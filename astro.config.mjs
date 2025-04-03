98214c14f79060f51214413777e21dd25726ded1
Cody Searles <cody@Codys-MacBook-Air.local>
2025-04-02 22:41:11 -0400

    Added vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
+  output: 'server',
+  site: 'https://aarbaa.com',
   server: {
     host: '0.0.0.0',
     port: 5000
   },
-  site: 'https://aarbaa.com',
   vite: {
     server: {
       cors: true,
@@ -17,7 +20,11 @@ export default defineConfig({
       watch: {
         usePolling: true
       },
-      allowedHosts: ['c60dd613-ceed-47e8-86b4-f18b84138f5b-00-lfo5g3iko6gr.picard.replit.dev', 'localhost', 'all']
+      allowedHosts: [
+        'c60dd613-ceed-47e8-86b4-f18b84138f5b-00-lfo5g3iko6gr.picard.replit.dev',
+        'localhost',
+        'all'
+      ]
     }
   },
   integrations: []
